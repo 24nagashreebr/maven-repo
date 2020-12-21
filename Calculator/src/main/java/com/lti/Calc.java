bc41cfc62fae9b93a9c0686ca020be67a4ef4aae
24nagashreebr <sendtonagashree24@gmail.com>
2020-12-21 15:16:57 +0530

    div method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -13,13 +13,17 @@ public class Calc {
 	public static int pro(int num1,int num2){
 		return num1*num2;
 	}
+	
+	public static int div(int num1,int num2){
+		return num1/num2;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(add(10,20));
 		System.out.println(subs(10,20));
 		System.out.println(pro(10,20));
-		
+		System.out.println(div(10,20));
 	}
 
 }
